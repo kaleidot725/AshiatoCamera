65e3be2710b129a9e73d7b86057252636cf518ae
kaleidot725 <hakodate.katsuragawa.yusuke@gmail.com>
2019-05-05 00:39:50 +0900

    Add Setting

--- src/app/src/main/java/kaleidot725/highestpeaks/setting/SettingActivity.kt
+++ src/app/src/main/java/kaleidot725/highestpeaks/setting/SettingActivity.kt
@@ -10,11 +10,8 @@ class SettingActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.setting_activity)
-
         if (savedInstanceState == null) {
-            supportFragmentManager.beginTransaction()
-                .replace(R.id.container, SettingFragment.newInstance())
-                .commitNow()
+            supportFragmentManager.beginTransaction().replace(R.id.container, SettingFragment.newInstance()).commitNow()
         }
     }
 }
