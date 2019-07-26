dfd867a04dc0ecb2a271f81f79a53843fcb8e5a7
kaleidot725 <hakodate.katsuragawa.yusuke@gmail.com>
2019-07-26 21:00:36 +0900

    Remove initialized flag

--- src/app/src/main/java/kaleidot725/highestpeaks/di/repository/DeveloperRepositoryImpl.kt
+++ src/app/src/main/java/kaleidot725/highestpeaks/di/repository/DeveloperRepositoryImpl.kt
@@ -6,7 +6,6 @@ import java.lang.IllegalStateException
 
 class DeveloperRepositoryImpl : DeveloperRepository {
     private val list : ArrayList<Developer> = ObservableArrayList<Developer>()
-    private var initialized : Boolean = false
 
     init {
         list.add(
@@ -17,22 +16,13 @@ class DeveloperRepositoryImpl : DeveloperRepository {
                 "hakodate.katsuragawa.yusuke@gmail.com"
             )
         )
-        initialized = true
     }
 
     override fun all(): List<Developer> {
-        if (!initialized) {
-            throw IllegalStateException("not initialized")
-        }
-
         return this.list.toList()
     }
 
     override fun count(): Int {
-        if (!initialized) {
-            throw IllegalStateException("not initialized")
-        }
-
         return this.list.count()
     }
 }
