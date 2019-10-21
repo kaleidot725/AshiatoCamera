0566081878028a8bdca1dd6b0eb7c54b61689b9a
kaleidot725 <hakodate.katsuragawa.yusuke@gmail.com>
2019-10-21 13:57:21 +0900

    Fix Draw Font Stroke Size
    StrokeSizeが固定値だったがFontSizeによって変化するようにした。

--- src/app/src/main/java/kaleidot725/ashiato/di/service/picture/DrawableCanvasImpl.kt
+++ src/app/src/main/java/kaleidot725/ashiato/di/service/picture/DrawableCanvasImpl.kt
@@ -21,15 +21,7 @@ class DrawableCanvasImpl() : DrawableCanvas {
     }
 
     override fun draw(x: Float, y: Float, text: String, color: Int, size: Float) {
-        val strokePaint = Paint().also {
-            it.color = android.graphics.Color.WHITE
-            it.textSize = size
-            it.isAntiAlias = true
-            it.strokeWidth = 2f
-            it.strokeJoin = Paint.Join.ROUND
-            it.strokeCap = Paint.Cap.ROUND
-            it.style = Paint.Style.STROKE
-        }
+        val strokePaint = createStrokePaint(size)
         canvas.drawText(text, x, y, strokePaint)
 
         val textPaint = Paint().also {
@@ -42,16 +34,13 @@ class DrawableCanvasImpl() : DrawableCanvas {
     }
 
     override fun draw(pos: PositionType, text: String, color: Int, size: Float) {
-       val strokePaint = Paint().also {
-            it.color = android.graphics.Color.WHITE
-            it.textSize = size
-            it.isAntiAlias = true
-            it.strokeWidth = 2f
-            it.strokeJoin = Paint.Join.ROUND
-            it.strokeCap = Paint.Cap.ROUND
-            it.style = Paint.Style.STROKE
-        }
-        canvas.drawText(text, calcX(pos, text, strokePaint), calcY(pos, text, strokePaint), strokePaint)
+        val strokePaint = createStrokePaint(size)
+        canvas.drawText(
+            text,
+            calcX(pos, text, strokePaint),
+            calcY(pos, text, strokePaint),
+            strokePaint
+        )
 
         val textPaint = Paint().also {
             it.color = color
@@ -60,46 +49,6 @@ class DrawableCanvasImpl() : DrawableCanvas {
         }
 
         canvas.drawText(text, calcX(pos, text, textPaint), calcY(pos, text, textPaint), textPaint)
-    }
-
-    private fun calcX(position: PositionType, text: String, paint: Paint): Float {
-        val bounds = Rect().also {
-            paint.getTextBounds(text, 0, text.length, it)
-        }
-
-        var margin = if (text.isEmpty()) { 0f }  else {bounds.width().toFloat() / text.length.toFloat() / 2f}
-
-        when (position) {
-            PositionType.TopLeft -> return 0f
-            PositionType.TopCenter -> return (canvas.width / 2 - bounds.width() / 2).toFloat()
-            PositionType.TopRight -> return (canvas.width - bounds.width()).toFloat() - margin
-            PositionType.CenterLeft -> return 0f
-            PositionType.Center -> return (canvas.width / 2 - bounds.width() / 2).toFloat()
-            PositionType.CenterRight -> return (canvas.width - bounds.width()).toFloat()  - margin
-            PositionType.BottomLeft -> return 0f
-            PositionType.BottomCenter -> return (canvas.width / 2 - bounds.width() / 2).toFloat()
-            PositionType.BottomRight -> return (canvas.width - bounds.width()).toFloat()  - margin
-        }
-    }
-
-    private fun calcY(position: PositionType, text: String, paint: Paint): Float {
-        val bounds = Rect().also {
-            paint.getTextBounds(text, 0, text.length, it)
-        }
-
-        var margin = if (text.isEmpty()) { 0f }  else {bounds.height().toFloat() / 8f }
-
-        when (position) {
-            PositionType.TopLeft -> return bounds.height().toFloat()
-            PositionType.TopCenter -> return bounds.height().toFloat()
-            PositionType.TopRight -> return bounds.height().toFloat()
-            PositionType.CenterLeft -> return (canvas.height / 2 - bounds.height() / 2).toFloat()
-            PositionType.Center -> return (canvas.height / 2 - bounds.height() / 2).toFloat()
-            PositionType.CenterRight -> return (canvas.height / 2 - bounds.height() / 2).toFloat()
-            PositionType.BottomLeft -> return canvas.height.toFloat() - margin
-            PositionType.BottomCenter -> return canvas.height.toFloat() - margin
-            PositionType.BottomRight -> return canvas.height.toFloat() - margin
-        }
     }
 
     override fun rotation(degree: Float) {
@@ -117,5 +66,67 @@ class DrawableCanvasImpl() : DrawableCanvas {
 
     override fun delete(path: String) {
         File(path).delete()
+    }
+
+    private fun createStrokePaint(fontSize: Float): Paint = Paint().also {
+        it.color = android.graphics.Color.WHITE
+        it.textSize = fontSize
+        it.isAntiAlias = true
+        it.strokeWidth = calcStrokeWidth(fontSize)
+        it.strokeJoin = Paint.Join.ROUND
+        it.strokeCap = Paint.Cap.ROUND
+        it.style = Paint.Style.STROKE
+    }
+
+    private fun calcStrokeWidth(fontSize: Float): Float {
+        return fontSize / 100f
+    }
+
+    private fun calcX(position: PositionType, text: String, paint: Paint): Float {
+        val bounds = Rect().also {
+            paint.getTextBounds(text, 0, text.length, it)
+        }
+
+        var margin = if (text.isEmpty()) {
+            0f
+        } else {
+            bounds.width().toFloat() / text.length.toFloat() / 2f
+        }
+
+        when (position) {
+            PositionType.TopLeft -> return 0f
+            PositionType.TopCenter -> return (canvas.width / 2 - bounds.width() / 2).toFloat()
+            PositionType.TopRight -> return (canvas.width - bounds.width()).toFloat() - margin
+            PositionType.CenterLeft -> return 0f
+            PositionType.Center -> return (canvas.width / 2 - bounds.width() / 2).toFloat()
+            PositionType.CenterRight -> return (canvas.width - bounds.width()).toFloat() - margin
+            PositionType.BottomLeft -> return 0f
+            PositionType.BottomCenter -> return (canvas.width / 2 - bounds.width() / 2).toFloat()
+            PositionType.BottomRight -> return (canvas.width - bounds.width()).toFloat() - margin
+        }
+    }
+
+    private fun calcY(position: PositionType, text: String, paint: Paint): Float {
+        val bounds = Rect().also {
+            paint.getTextBounds(text, 0, text.length, it)
+        }
+
+        var margin = if (text.isEmpty()) {
+            0f
+        } else {
+            bounds.height().toFloat() / 8f
+        }
+
+        when (position) {
+            PositionType.TopLeft -> return bounds.height().toFloat()
+            PositionType.TopCenter -> return bounds.height().toFloat()
+            PositionType.TopRight -> return bounds.height().toFloat()
+            PositionType.CenterLeft -> return (canvas.height / 2 - bounds.height() / 2).toFloat()
+            PositionType.Center -> return (canvas.height / 2 - bounds.height() / 2).toFloat()
+            PositionType.CenterRight -> return (canvas.height / 2 - bounds.height() / 2).toFloat()
+            PositionType.BottomLeft -> return canvas.height.toFloat() - margin
+            PositionType.BottomCenter -> return canvas.height.toFloat() - margin
+            PositionType.BottomRight -> return canvas.height.toFloat() - margin
+        }
     }
 }
