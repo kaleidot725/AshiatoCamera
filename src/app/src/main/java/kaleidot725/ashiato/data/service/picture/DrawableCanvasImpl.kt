d508be3060043534255611ed658793751f07f22f
kaleidot725 <hakodate.katsuragawa.yusuke@gmail.com>
2019-12-04 23:57:05 +0900

    Add Margin Position

--- src/app/src/main/java/kaleidot725/ashiato/data/service/picture/DrawableCanvasImpl.kt
+++ src/app/src/main/java/kaleidot725/ashiato/data/service/picture/DrawableCanvasImpl.kt
@@ -9,6 +9,7 @@ class DrawableCanvasImpl() : DrawableCanvas {
     private var currentPath: String = ""
     private lateinit var bitmap: Bitmap
     private lateinit var canvas: Canvas
+    private val margin = 10f
 
     override fun load(path: String) {
         val options = BitmapFactory.Options().also {
@@ -87,22 +88,22 @@ class DrawableCanvasImpl() : DrawableCanvas {
             paint.getTextBounds(text, 0, text.length, it)
         }
 
-        var margin = if (text.isEmpty()) {
+        val centerlize = if (text.isEmpty()) {
             0f
         } else {
             bounds.width().toFloat() / text.length.toFloat() / 2f
         }
 
         when (position) {
-            PositionType.TopLeft -> return 0f
+            PositionType.TopLeft -> return margin
             PositionType.TopCenter -> return (canvas.width / 2 - bounds.width() / 2).toFloat()
-            PositionType.TopRight -> return (canvas.width - bounds.width()).toFloat() - margin
-            PositionType.CenterLeft -> return 0f
+            PositionType.TopRight -> return (canvas.width - bounds.width()).toFloat() - centerlize - margin
+            PositionType.CenterLeft -> return margin
             PositionType.Center -> return (canvas.width / 2 - bounds.width() / 2).toFloat()
-            PositionType.CenterRight -> return (canvas.width - bounds.width()).toFloat() - margin
-            PositionType.BottomLeft -> return 0f
+            PositionType.CenterRight -> return (canvas.width - bounds.width()).toFloat() - centerlize - margin
+            PositionType.BottomLeft -> return margin
             PositionType.BottomCenter -> return (canvas.width / 2 - bounds.width() / 2).toFloat()
-            PositionType.BottomRight -> return (canvas.width - bounds.width()).toFloat() - margin
+            PositionType.BottomRight -> return (canvas.width - bounds.width()).toFloat() - centerlize - margin
         }
     }
 
@@ -111,22 +112,22 @@ class DrawableCanvasImpl() : DrawableCanvas {
             paint.getTextBounds(text, 0, text.length, it)
         }
 
-        var margin = if (text.isEmpty()) {
+        var centerlize = if (text.isEmpty()) {
             0f
         } else {
             bounds.height().toFloat() / 8f
         }
 
         when (position) {
-            PositionType.TopLeft -> return bounds.height().toFloat()
-            PositionType.TopCenter -> return bounds.height().toFloat()
-            PositionType.TopRight -> return bounds.height().toFloat()
+            PositionType.TopLeft -> return bounds.height().toFloat() + margin
+            PositionType.TopCenter -> return bounds.height().toFloat() + margin
+            PositionType.TopRight -> return bounds.height().toFloat() + margin
             PositionType.CenterLeft -> return (canvas.height / 2 - bounds.height() / 2).toFloat()
             PositionType.Center -> return (canvas.height / 2 - bounds.height() / 2).toFloat()
             PositionType.CenterRight -> return (canvas.height / 2 - bounds.height() / 2).toFloat()
-            PositionType.BottomLeft -> return canvas.height.toFloat() - margin
-            PositionType.BottomCenter -> return canvas.height.toFloat() - margin
-            PositionType.BottomRight -> return canvas.height.toFloat() - margin
+            PositionType.BottomLeft -> return canvas.height.toFloat() - centerlize - margin
+            PositionType.BottomCenter -> return canvas.height.toFloat() - centerlize - margin
+            PositionType.BottomRight -> return canvas.height.toFloat() - centerlize - margin
         }
     }
 }
