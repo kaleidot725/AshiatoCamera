181e9638f26ac010fef50fe54d38c16eefd8aa74
kaleidot725 <hakodate.katsuragawa.yusuke@gmail.com>
2019-12-12 08:39:07 +0900

    Refactor RotationRecyclerViewModel

--- src/app/src/main/java/kaleidot725/ashiato/ui/edit/rotation/RotationRecyclerViewModel.kt
+++ src/app/src/main/java/kaleidot725/ashiato/ui/edit/rotation/RotationRecyclerViewModel.kt
@@ -9,6 +9,7 @@ import io.reactivex.disposables.CompositeDisposable
 import kaleidot725.ashiato.data.service.picture.Angle
 import kaleidot725.ashiato.data.service.picture.PictureEditor
 import kaleidot725.ashiato.data.service.picture.RotationEditor
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 
 class RotationRecyclerViewModel(
@@ -16,24 +17,23 @@ class RotationRecyclerViewModel(
     private val rotationEditor: RotationEditor,
     private val angle: Angle
 ) : ViewModel() {
-    private val _detail: MutableLiveData<String> = MutableLiveData()
-    val detail: LiveData<String> get() = _detail
-
-    private val _enabled: MutableLiveData<Boolean> = MutableLiveData()
-    val enabled: LiveData<Boolean> get() = _enabled
-
     private val compositeDisposable = CompositeDisposable()
 
-    init {
-        _detail.value = angle.detail
-        _enabled.value = (rotationEditor.lastEnabled == angle)
+    private val _detail: MutableLiveData<String> = MutableLiveData<String>().apply {
+        postValue(angle.detail)
+    }
+    val detail: LiveData<String> get() = _detail
+
+    private val _enabled: MutableLiveData<Boolean> = MutableLiveData<Boolean>().apply {
+        postValue(isEnabled(angle))
         compositeDisposable.add(
-            rotationEditor.enabled.subscribe { _enabled.postValue(it == angle) }
+            rotationEditor.enabled.subscribe { postValue(isEnabled(angle)) }
         )
     }
+    val enabled: LiveData<Boolean> get() = _enabled
 
     fun click(v: View) {
-        viewModelScope.launch {
+        viewModelScope.launch(Dispatchers.Default) {
             rotationEditor.enable(angle)
             pictureEditor.modifyRotation(angle.value)
             pictureEditor.commit()
@@ -43,5 +43,9 @@ class RotationRecyclerViewModel(
     override fun onCleared() {
         super.onCleared()
         compositeDisposable.dispose()
+    }
+
+    private fun isEnabled(angle: Angle): Boolean {
+        return (rotationEditor.lastEnabled == angle)
     }
 }
