2bc32098b233082d380de7965eee4c153377b944
kaleidot725 <hakodate.katsuragawa.yusuke@gmail.com>
2019-12-12 08:11:06 +0900

    Refactor ColorRecyclerViewModel

--- src/app/src/main/java/kaleidot725/ashiato/ui/edit/color/ColorRecyclerViewModel.kt
+++ src/app/src/main/java/kaleidot725/ashiato/ui/edit/color/ColorRecyclerViewModel.kt
@@ -9,6 +9,7 @@ import io.reactivex.disposables.CompositeDisposable
 import kaleidot725.ashiato.data.service.picture.Color
 import kaleidot725.ashiato.data.service.picture.ColorEditor
 import kaleidot725.ashiato.data.service.picture.PictureEditor
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 
 class ColorRecyclerViewModel(
@@ -16,30 +17,29 @@ class ColorRecyclerViewModel(
     private val colorEditor: ColorEditor,
     private val color: Color
 ) : ViewModel() {
-
-    private val _detail: MutableLiveData<String> = MutableLiveData()
+    private val compositeDisposable = CompositeDisposable()
+    private val _detail: MutableLiveData<String> = MutableLiveData<String>().apply {
+        value = color.detail
+    }
     val detail: LiveData<String> get() = _detail
 
-    private val _txtColor: MutableLiveData<Int> = MutableLiveData()
+    private val _txtColor: MutableLiveData<Int> = MutableLiveData<Int>().apply {
+        value = selectBackgroundColor(color)
+    }
     val txtColor: LiveData<Int> get() = _txtColor
 
-    private val _enabled: MutableLiveData<Boolean> = MutableLiveData()
-    val enabled: LiveData<Boolean> get() = _enabled
-
-    private val compositeDisposable = CompositeDisposable()
-
-    init {
-        _txtColor.value =
-            if (color.value == android.graphics.Color.WHITE) android.graphics.Color.BLACK else color.value
-        _detail.value = color.detail
-        _enabled.value = (colorEditor.lastEnabled == color)
+    private val _enabled: MutableLiveData<Boolean> = MutableLiveData<Boolean>().apply {
+        value = isSelectedColor(color)
         compositeDisposable.add(
-            colorEditor.enabled.subscribe { _enabled.postValue((it == color)) }
+            colorEditor.enabled.subscribe {
+                postValue(isSelectedColor(color))
+            }
         )
     }
+    val enabled: LiveData<Boolean> get() = _enabled
 
     fun click(v: View) {
-        viewModelScope.launch {
+        viewModelScope.launch(Dispatchers.Default) {
             colorEditor.enable(color)
             pictureEditor.modifyColor(color.value)
             pictureEditor.commit()
@@ -49,5 +49,17 @@ class ColorRecyclerViewModel(
     override fun onCleared() {
         super.onCleared()
         compositeDisposable.dispose()
+    }
+
+    private fun selectBackgroundColor(thisColor: Color): Int {
+        return if (thisColor.value == android.graphics.Color.WHITE) {
+            android.graphics.Color.BLACK
+        } else {
+            thisColor.value
+        }
+    }
+
+    private fun isSelectedColor(thisColor: Color): Boolean {
+        return (colorEditor.lastEnabled == thisColor)
     }
 }
