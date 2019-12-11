04b64955b046f3b49522514d8eb98ca505751440
kaleidot725 <hakodate.katsuragawa.yusuke@gmail.com>
2019-12-12 08:30:15 +0900

    Refactor PositionViewModel

--- src/app/src/main/java/kaleidot725/ashiato/ui/edit/position/PositionViewModel.kt
+++ src/app/src/main/java/kaleidot725/ashiato/ui/edit/position/PositionViewModel.kt
@@ -7,6 +7,7 @@ import androidx.lifecycle.viewModelScope
 import kaleidot725.ashiato.data.repository.PositionRepository
 import kaleidot725.ashiato.data.service.picture.PictureEditor
 import kaleidot725.ashiato.data.service.picture.PositionEditor
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 
 class PositionViewModel(
@@ -20,14 +21,11 @@ class PositionViewModel(
     val positionRecyclerViewModels: LiveData<List<PositionRecyclerViewModel>> get() = _positionRecyclerViewModels
 
     fun load() {
-        viewModelScope.launch {
-            val all = positionRepository.all()
-            val vms = mutableListOf<PositionRecyclerViewModel>()
-            for (position in all) {
-                vms.add(PositionRecyclerViewModel(pictureEditor, positionEditor, position))
+        viewModelScope.launch(Dispatchers.Default) {
+            val vms = positionRepository.all().map {
+                PositionRecyclerViewModel(pictureEditor, positionEditor, it)
             }
-
-            _positionRecyclerViewModels.value = vms
+            _positionRecyclerViewModels.postValue(vms)
         }
     }
 }
