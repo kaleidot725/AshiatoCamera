67f0493b46ab754bbf62d3f5edcf1ff009498081
kaleidot725 <hakodate.katsuragawa.yusuke@gmail.com>
2020-02-04 00:11:29 +0900

    Refatctor SettingList

--- src/app/src/main/java/kaleidot725/ashiato/ui/main/settinglist/SettingListViewModel.kt
+++ src/app/src/main/java/kaleidot725/ashiato/ui/main/settinglist/SettingListViewModel.kt
@@ -7,19 +7,10 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import kaleidot725.ashiato.R
 import kaleidot725.ashiato.data.repository.MenuRepository
+import kaleidot725.ashiato.data.service.contact.Menu
 import kotlinx.coroutines.launch
 
-class SettingListViewModel(
-    private val context: Context,
-    private val menuRepository: MenuRepository
-) : ViewModel() {
-    enum class NavEvent {
-        Setting,
-        License,
-        Contact,
-        Privacy,
-    }
-
+class SettingListViewModel(private val context: Context, private val menuRepository: MenuRepository) : ViewModel() {
     private val _menus: MutableLiveData<List<MenuViewModel>> = MutableLiveData()
     val menus: LiveData<List<MenuViewModel>> get() = _menus
 
@@ -28,17 +19,24 @@ class SettingListViewModel(
 
     fun load() {
         viewModelScope.launch {
-            val vms = menuRepository.all().map {
-                MenuViewModel(context, it) { menu ->
-                    when (menu.title) {
-                        context.getString(R.string.menu_setting) -> _event.postValue(NavEvent.Setting)
-                        context.getString(R.string.menu_license) -> _event.postValue(NavEvent.License)
-                        context.getString(R.string.menu_contanct) -> _event.postValue(NavEvent.Contact)
-                        context.getString(R.string.menu_privacy) -> _event.postValue(NavEvent.Privacy)
-                    }
-                }
-            }
+            val vms = menuRepository.all().map { MenuViewModel(it) { menu -> navigateMenu(menu) } }
             _menus.postValue(vms)
         }
+    }
+
+    private fun navigateMenu(menu: Menu) {
+        when (menu.title) {
+            context.getString(R.string.menu_setting) -> _event.postValue(NavEvent.Setting)
+            context.getString(R.string.menu_license) -> _event.postValue(NavEvent.License)
+            context.getString(R.string.menu_contanct) -> _event.postValue(NavEvent.Contact)
+            context.getString(R.string.menu_privacy) -> _event.postValue(NavEvent.Privacy)
+        }
+    }
+
+    enum class NavEvent {
+        Setting,
+        License,
+        Contact,
+        Privacy,
     }
 }
