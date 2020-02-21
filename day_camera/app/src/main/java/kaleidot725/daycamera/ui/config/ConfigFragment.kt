d20799def44425450c0f8b6caf73b7e503d8e4bd
kaleidot725 <hakodate.katsuragawa.yusuke@gmail.com>
2020-02-21 21:55:40 +0900

    Add Preference Setting

--- day_camera/app/src/main/java/kaleidot725/daycamera/ui/config/ConfigFragment.kt
+++ day_camera/app/src/main/java/kaleidot725/daycamera/ui/config/ConfigFragment.kt
@@ -2,23 +2,11 @@ package kaleidot725.daycamera.ui.config
 
 
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-import androidx.databinding.DataBindingUtil
-import androidx.fragment.app.Fragment
+import androidx.preference.PreferenceFragmentCompat
 import kaleidot725.daycamera.R
-import kaleidot725.daycamera.databinding.FragmentConfigBinding
 
-class ConfigFragment : Fragment() {
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        return DataBindingUtil.inflate<FragmentConfigBinding>(inflater, LAYOUT_ID, container, false).apply {
-            viewModel = ConfigViewModel()
-            lifecycleOwner = viewLifecycleOwner
-        }.root
-    }
-
-    companion object {
-        private const val LAYOUT_ID = R.layout.fragment_config
+class ConfigFragment : PreferenceFragmentCompat() {
+    override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
+        setPreferencesFromResource(R.xml.preferences, rootKey)
     }
 }
